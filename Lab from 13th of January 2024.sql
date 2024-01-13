f44f36c1ca0dfd9ce0cd72c0cd4e0c453cb7ca11
Laura Clark <clark@Lauras-MacBook-Air.local>
2024-01-13 13:40:12 +0100

    finished

---
+++ Lab from 13th of January 2024.sql
@@ -0,0 +1,7 @@
+-- Lab 4 Unit 3
+-- 1. Use dbdiagram.io or draw.io to propose a new structure for the Sakila database
+
+
+-- 2. Define primary keys and foreign keys for the new database.
+-- i created a new table called info_rentals, which came from the film table but just keeping the columns: customer_id, rental_id and film_id
+-- here the primary key is customer_id and then rental_id is a foreign key for the rental table as well as film_id is a foreign key for the film table
